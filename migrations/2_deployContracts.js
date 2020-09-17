14bcf4238c835afeadefa9d7c3fe69e572addafc
alikemalcelenk <alikemalcelenkk@gmail.com>
2020-09-17 21:29:42 +0300

    deploy contracts

--- migrations/2_deployContracts.js
+++ migrations/2_deployContracts.js
@@ -1,5 +1,25 @@
 const TokenFarm = artifacts.require("TokenFarm");
+const AkcToken = artifacts.require("AkcToken");
+const DaiToken = artifacts.require("DaiToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function (deployer, network, accounts) {
+    //Deploy Akc Token
+    await deployer.deploy(AkcToken);
+    const akcToken = await AkcToken.deployed()
+
+    //Deploy Dai Token
+    await deployer.deploy(DaiToken);
+    const daiToken = await DaiToken.deployed()
+
+    //Deploy Token Farm
+    await deployer.deploy(TokenFarm, akcToken.address, daiToken.address);
+    const tokenFarm = await TokenFarm.deployed()
+
+    // Transfer all tokens to TokenFarm (1 million)
+    await akcToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+    // Transfer 100 Mock DAI tokens to investor
+    //accounts[0] -> deployer adress
+    //accounts[1] -> investor adress
+    await daiToken.transfer(accounts[1], '100000000000000000000')
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.5.0;
 
+import "./AkcToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
-    string public name = 'AKC Token Farm';
-} 
+    string public name = "Akc Token Farm";
+    AkcToken public akcToken;
+    DaiToken public daiToken;
+
+    constructor(AkcToken _akcToken, DaiToken _daiToken) public {
+        akcToken = _akcToken;
+        daiToken = _daiToken;
+    }
+}
