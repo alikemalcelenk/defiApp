aa99caeedd889a594ec5df0874b010c6bd52a1ef
alikemalcelenk <alikemalcelenkk@gmail.com>
2020-09-19 02:35:09 +0300

    unstakeToken and test this

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -11,7 +11,7 @@ contract TokenFarm {
 
     // mapping = key->value
     address[] public stakers;
-    mapping(address => uint) public stakingBalance;
+    mapping(address => uint256) public stakingBalance;
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
 
@@ -21,19 +21,19 @@ contract TokenFarm {
         owner = msg.sender;
     }
 
-    function stakeTokens(uint _amount) public {
+    function stakeTokens(uint256 _amount) public {
         // Require amount greater than 0
         require(_amount > 0, "amount cannot be 0");
 
         // Trasnfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
         //this = bu akıllı sözleşmenin kendisine karşılık gelir.
-      
+
         // Update staking balance
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
         //Kullanıcı daha önceden stake yapmadıysa onu stakers arr sine ekliyorum
-        if(!hasStaked[msg.sender]) {
+        if (!hasStaked[msg.sender]) {
             stakers.push(msg.sender);
         }
 
@@ -42,17 +42,35 @@ contract TokenFarm {
         hasStaked[msg.sender] = true;
     }
 
-     function issueTokens() public {
+    function issueTokens() public {
         // Only owner can call this function
         require(msg.sender == owner, "caller must be the owner");
 
         // Issue tokens to all stakers
-        for (uint i=0; i<stakers.length; i++) {
+        for (uint256 i = 0; i < stakers.length; i++) {
             address recipient = stakers[i];
-            uint balance = stakingBalance[recipient];
-            if(balance > 0) {
+            uint256 balance = stakingBalance[recipient];
+            if (balance > 0) {
                 akcToken.transfer(recipient, balance);
             }
         }
+    }
+
+    // Unstaking Tokens (Withdraw)
+    function unstakeTokens() public {
+        // Fetch staking balance
+        uint256 balance = stakingBalance[msg.sender];
+
+        // Require amount greater than 0
+        require(balance > 0, "staking balance cannot be 0");
+
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transfer(msg.sender, balance);
+
+        // Reset staking balance
+        stakingBalance[msg.sender] = 0;
+
+        // Update staking status
+        isStaking[msg.sender] = false;
     }
 }
